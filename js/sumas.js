b42683950fe53248ff63a0ec180f31ae5bc3ee9c
Exvalivar <varoal07_acm@hotmail.com>
2017-04-03 15:05:06 -0600

    Ejemplo01 javascript

---
+++ js/sumas.js
@@ -0,0 +1,15 @@
+function suma(a,b){
+	return a+b;
+}
+var suma2=function (a,b) {
+	return a+b
+}
+function suma3(){
+	var a=document.getElementById("texto1").value
+	var b=document.getElementById("texto2").value
+	return parseInt(a)+parseInt(b); 
+}
+
+function texto(){
+	alert(document.getElementById("texto1").value)
+}
